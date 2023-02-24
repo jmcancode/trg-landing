65dde92065b52147b49bc28d98a6407ff5b23bd3
jmcancode <jm@bboycreative.com>
2023-02-24 17:08:10 -0600

    2023 update

--- src/App.js
+++ src/App.js
@@ -1,79 +1,110 @@
-import React, {Fragment, useRef} from 'react';
-import {Button} from 'react-bootstrap';
+import React, { Fragment, useRef } from "react";
+import { Button } from "react-bootstrap";
 // styles
-import "./app.css"
+import "./app.css";
 // components
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
 
 function App() {
+  const decreeRef = useRef(null);
+  const garmentRef = useRef(null);
+  const executeRef = () => scrollToRef(decreeRef);
+  const executeRefTwo = () => scrollToRef(garmentRef);
 
-    const decreeRef = useRef(null);
-    const garmentRef = useRef(null);
-    const executeRef = () => scrollToRef(decreeRef)
-    const executeRefTwo = () => scrollToRef(garmentRef);
-
-    return (
-        <Fragment>
-            <section className="hero">
-                <div className="app"></div>
-                <div className="royal-decree-btn">
-                    <Button onClick={executeRef} variant="outline-dark">Learn more</Button>
-                </div>
-            </section>
-            <section ref={decreeRef} className="announcements">
-                <div>
-                    <h1 className="royal-decree">By Royal Decree</h1>
-                </div>
-                <div>
-                    <p className="royal-text">
-                        Summer of 2022, we will re-introduce made-to-order suits, jackets, and trousers
-                        in tandem with our shirt subscription service, Cuff & Collar.
-                        <br/><br/>
-                        At this time, access to our entry level membership is only by way of invitation
-                        from an active founding member or directly from a member of our team. For more
-                        information, please download our pitch
-                        <a
-                            href="general.pdf"
-                            style={{
-                            padding: '0px 2.5px',
-                            textDecoration: 'none',
-                            color: '#ededed'
-                        }}
-                            download>here.</a>
-
-                    </p>
-
-                </div>
-                <div>
-                    <Button onClick={executeRefTwo} variant="outline-light">Garment Program</Button>
-                </div>
-            </section>
-            <section ref={garmentRef} className="cuffsncollars">
-                <div className="cnc-card">
-                    <div
-                        style={{
-                        padding: '10px 50px'
-                    }}>
-                        <h1
-                            style={{
-                            color: '#9a1e28',
-                            textTransform: 'uppercase'
-                        }}>The Royal Guard Shop</h1>
-                    </div>
-                    <div>
-                        <p>Something for everyone. Launching Fall of 2022</p>
-                    </div>
-                    <div style={{padding: '50px 0px'}}>
-                        <Button onClick={() => alert('Ooops, theroyalguard.shop is under construction. Please check back in a week.')} variant="dark">theroyalgaurd.shop</Button>
-                    </div>
-                </div>
-
-            </section>
-            <Footer/>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <section className="hero">
+        <div className="app"></div>
+        <div className="royal-decree-btn">
+          <Button onClick={executeRef} variant="outline-dark">
+            Learn more
+          </Button>
+        </div>
+      </section>
+      <section ref={decreeRef} className="announcements">
+        <div>
+          <h1 className="royal-decree">By Royal Decree</h1>
+        </div>
+        <div>
+          <p className="royal-text">
+            Summer of 2022, we will re-introduce made-to-order suits, jackets,
+            and trousers in tandem with our shirt subscription service, Cuff &
+            Collar.
+            <br />
+            <br />
+            At this time, access to our entry level membership is only by way of
+            invitation from an active founding member or directly from a member
+            of our team. For more information, please download our pitch
+            <a
+              href="general.pdf"
+              style={{
+                padding: "0px 2.5px",
+                textDecoration: "none",
+                color: "#ededed",
+              }}
+              download
+            >
+              here.
+            </a>
+          </p>
+        </div>
+        <div>
+          <Button onClick={executeRefTwo} variant="outline-light">
+            Garment Program
+          </Button>
+        </div>
+      </section>
+      <section ref={garmentRef} className="cuffsncollars">
+        <div className="cnc-card">
+          <div
+            style={{
+              padding: "10px 50px",
+            }}
+          >
+            <h1
+              style={{
+                color: "#9a1e28",
+                textTransform: "uppercase",
+                textAlign: "center",
+              }}
+            >
+              The Royal Guard Mobile
+            </h1>
+          </div>
+          <div
+            style={{
+              padding: "10px 50px",
+              textAlign: "center",
+            }}
+          >
+            <p
+              style={{
+                color: "#fff",
+                fontWeight: "bold",
+                fontSize: "1.25rem",
+              }}
+            >
+              Something for everyone. Launching Spring of 2023
+            </p>
+          </div>
+          <div style={{ padding: "50px 0px" }}>
+            <Button
+              variant="dark"
+              onClick={() => {
+                window.location.href =
+                  "https://apps.apple.com/us/app/cuff-and-collar/id1561239176";
+              }}
+            >
+              Download The App
+            </Button>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
+import React from "react";
 // styles
-import "./footer.css"
+import "./footer.css";
 // images
-import logo from '../assets/images/trg-gfdl.png'
+import logo from "../assets/images/trg-gfdl.png";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="first-section">
-              <img className="img-responsive" src={logo} alt="logo" height={100} width={100}/>
-            </div>
-            <div className="second-section">
-                <ul>
-                    <li>FAQs</li>
-                    <li>Press</li>
-                    <li>Contact</li>
-                    <li>
-                      <a style={{textDecoration: 'none', color: "#171717"}} href="https://stylebyjm.com/" target="_blank" rel="noopener noreferrer">SBJM Footwear
-                        </a></li>
-                </ul>
-            </div>
-            <div className="third-section">
-              <ul>
-                <li>Terms of Service</li>
-                <li>Privacy Policy</li>
-                <li>Web 3.0</li>
-              </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <div className="first-section animate-bottom">
+        <img
+          className="img-responsive"
+          src={logo}
+          alt="logo"
+          height={80}
+          width={80}
+        />
+      </div>
+      <div className="second-section animate-bottom">
+        <ul>
+          <li>FAQs</li>
+        </ul>
+      </div>
+      <div className="third-section animate-bottom">
+        <ul>
+          <li>Legal Notices</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
